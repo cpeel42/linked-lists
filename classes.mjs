f0ffd89b73bdf6534e1b63015e2469aa97c4eb24
cpeel42 <175068649+cpeel42@users.noreply.github.com>
2024-12-07 19:39:02 -0500

    Refactored code. Use methods in other methods when possible to avoid duplication

--- classes.mjs
+++ classes.mjs
@@ -1,149 +1,102 @@
 class Node {
   constructor(value) {
-    this.value = value
-    this.nextNode = null 
+    this.value = value;
+    this.nextNode = null;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.firstNode = null
+    this.firstNode = null;
+    this.tail = null;
   }
 
   append(value) {
-    // adds a new node containing value to the end of the list
-    const newNode = new Node(value)
-    if (this.firstNode) {
-      let currentNode = this.firstNode
-      while (currentNode.nextNode) {
-        currentNode = currentNode.nextNode
-      }
-      currentNode.nextNode = newNode
-    } else { 
-      this.firstNode = newNode
+    const newNode = new Node(value);
+    if (!this.firstNode) {
+      this.firstNode = this.tail = newNode;
+    } else {
+      this.tail.nextNode = newNode;
+      this.tail = newNode;
     }
   }
 
   prepend(value) {
-    // adds a new node containing value to the start of the list
-    const newNode = new Node(value)
-    if (this.firstNode) {
-      newNode.nextNode = this.firstNode
-      this.firstNode = newNode
-
-    } 
-    this.firstNode = newNode
+    const newNode = new Node(value);
+    newNode.nextNode = this.firstNode;
+    this.firstNode = newNode;
+    if (!this.tail) this.tail = newNode;
   }
 
   size() {
-    // returns the total number of nodes in the list
-    let count = 0
-    let currentNode = this.firstNode
+    let count = 0, currentNode = this.firstNode;
     while (currentNode) {
-      count += 1
-      currentNode = currentNode.nextNode
+      count++;
+      currentNode = currentNode.nextNode;
     }
-    return count
+    return count;
   }
 
   head() {
-    // returns the first node in the list
-    return this.firstNode
+    return this.firstNode;
   }
 
-  tail() {
-    // returns the last node in the list
-    let currentNode = this.firstNode.nextNode
-    while (currentNode.nextNode) {
-      currentNode = currentNode.nextNode
-    }
-    return currentNode
+  tailNode() {
+    return this.tail;
   }
 
   at(index) {
-    // returns the node at the given index
-    const errorMessage = `not found`
-    
-    if (this.firstNode) {
-      let currentNode = this.firstNode
-      for (let current = 0; current < index; current++) {
-        if (currentNode.nextNode) {
-          currentNode = currentNode.nextNode   
-        } else {
-          return errorMessage
-        }
-      }
-      return currentNode
-    } 
-    return errorMessage
+    if (index < 0) return null;
+
+    let currentNode = this.firstNode;
+    while (currentNode && index-- > 0) {
+      currentNode = currentNode.nextNode;
+    }
+    return currentNode || null;
   }
 
   pop() {
-    // removes the last element from the list
-    if (this.firstNode) {
-      let currentNode = this.firstNode
-      while (currentNode.nextNode.nextNode) {
-        currentNode = currentNode.nextNode
+    if (!this.firstNode) return;
+
+    if (this.firstNode === this.tail) {
+      this.firstNode = this.tail = null;
+    } else {
+      let currentNode = this.firstNode;
+      while (currentNode.nextNode !== this.tail) {
+        currentNode = currentNode.nextNode;
       }
-      currentNode.nextNode = null
+      currentNode.nextNode = null;
+      this.tail = currentNode;
     }
   }
 
   contains(value) {
-    // returns true if the passed in value is in the list and otherwise returns false.
-    if (this.firstNode) {
-      let currentNode = this.firstNode
-      while (currentNode) {
-        if (value == currentNode.value) {
-          return true
-        }
-        currentNode = currentNode.nextNode
-      }
+    let currentNode = this.firstNode;
+    while (currentNode) {
+      if (currentNode.value === value) return true;
+      currentNode = currentNode.nextNode;
     }
-    return false
+    return false;
   }
 
   find(value) {
-    // returns the index of the node containing value, or null if not found.
-    if (this.firstNode) {
-      let index = 0
-      let currentNode = this.firstNode
-      while (currentNode) {
-        if (value == currentNode.value) {
-          return index
-        }
-        index += 1
-        currentNode = currentNode.nextNode
-      }
+    let index = 0, currentNode = this.firstNode;
+    while (currentNode) {
+      if (currentNode.value === value) return index;
+      currentNode = currentNode.nextNode;
+      index++;
     }
-    return null
+    return null;
   }
 
   toString() {
-    // represents your LinkedList objects as strings, so you can print them out and preview them in the console. 
-    // The format should be: ( value ) -> ( value ) -> ( value ) -> null
-    let string = ''
-    if (this.firstNode) {
-      let currentNode = this.firstNode
-      while (currentNode) {
-        string += `${currentNode.value} -> `
-        currentNode = currentNode.nextNode
-      }
-      string += `null`
+    let result = "", currentNode = this.firstNode;
+    while (currentNode) {
+      result += `${currentNode.value} -> `;
+      currentNode = currentNode.nextNode;
     }
-    return string
+    return result + "null";
   }
-
-  //BONUS
-  //Tip: When you insert or remove a node, consider how it will affect the existing nodes. Some of the nodes will need their nextNode link updated.
-  insertAt(value, index) {
-    // inserts a new node with the provided value at the given index.
-  }
-
-  removeAt(index) {
-    // removes the node at the given index.
-  }
-
 }
 
-export { LinkedList }
+export { LinkedList };
